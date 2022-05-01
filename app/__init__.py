232565b118167533a92435c8c7dfa59bde03542d
BECKY-OCHOLLA <becky.ocholla@student.moringaschool.com>
2022-05-01 22:57:02 +0300

    it will update index.html and add rooting at views.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,9 @@
 from email.mime import application
 from flask import Flask
-# from newsapi import NewsApiClient
 
 
 
-#initializing application
+
 app=Flask(__name__)
 
 
--- app/views.py
+++ app/views.py
@@ -1,8 +1,10 @@
 from flask import render_template
 from app import app
 
-
 #views
 @app.route('/')
 def index():
-    return render_template('index.html')
+
+    articles = publishedArticles()
+
+    return  render_template('index.html', articles = articles)
