644aea3a08b66ae757116dedb8ba7a69d976cfaa
BECKY-OCHOLLA <becky.ocholla@student.moringaschool.com>
2022-05-02 01:13:29 +0300

    it will add new folders and files

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from app import app
-
+from .request import businessArticles, entArticles, get_news_source, healthArticles, publishedArticles, randomArticles, scienceArticles, sportArticles, techArticles, topHeadlines
 #views
 @app.route('/')
 def index():
